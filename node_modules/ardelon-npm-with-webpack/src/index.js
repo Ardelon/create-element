52579ada40a48bd0c5812209951813ed40869e1a
ardelon <akeskinw@gmail.com>
2022-07-06 11:19:41 +0300

    -

--- node_modules/ardelon-npm-with-webpack/src/index.js
+++
@@ -1,9 +0,0 @@
-const consoleFunction2 = require('./import')
-const consoleFunction = () => {
-    console.log("I don't believe that is actually work");
-    console.log("selam");
-    console.log(consoleFunction2);
-    consoleFunction2.consoleFunction2();
-}
-
-module.exports = consoleFunction
