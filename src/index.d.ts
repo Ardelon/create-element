52420af358e4b5ec6d5a1da49332e62df825f9a2
ardelon <akeskinw@gmail.com>
2023-02-20 16:16:54 +0300

    type file updated

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,12 @@
 interface Config {
-	tag: string;
-	classList: string[];
-	id: string;
-	attributeList: {};
-	innerText: string;
-	innerHTML: string;
-	src: string;
-	eventList: {};
+	tag?: string;
+	classList?: string[];
+	id?: string;
+	attributeList?: {};
+	innerText?: string;
+	innerHTML?: string;
+	src?: string;
+	eventList?: {};
 }
 
 declare module "ardelon-create-element" {
