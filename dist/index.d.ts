a41a2e14c807c781064d9b3fadc87553cd603afa
ardelon <akeskinw@gmail.com>
2023-02-20 16:45:22 +0300

    node modules

---
+++ dist/index.d.ts
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {object} config
+ * @param {string} config.tag
+ * @param {Array<String>} config.classList
+ * @param {String} config.id
+ * @param {object} config.attributeList
+ * @param {String} config.innerText
+ * @param {String} config.innerHTML
+ * @param {String} config.src
+ * @param {object} config.eventList
+ * @description Create a node element with given parameters and returns it. It is used for reducing code repeating and easy creation of elements
+ * @returns {Node object}
+ * @function createElement
+ */
+declare const createElement: (config: any) => any;
+//# sourceMappingURL=index.d.ts.map
